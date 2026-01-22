1610d11ad0b4d310b9b2e77783d00988a4a2fc61
MouadBenaich <mobenchiso@gmail.com>
2026-01-22 23:36:01 +0100

    Remove Clarifai-specific code, use Face++ only

--- src/App.js
+++ src/App.js
@@ -16,18 +16,6 @@ class App extends Component {
       box: {}
     };
   }
-  calculateFaceLocation = (data) => { 
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box; 
-    const image = document.getElementById("inputImage"); 
-    const width = Number(image.width); 
-    const height = Number(image.height); 
-    return { 
-      leftCol: clarifaiFace.left_col * width, 
-      topRow: clarifaiFace.top_row * height, 
-      rightCol: width - clarifaiFace.right_col * width, 
-      bottomRow: height - clarifaiFace.bottom_row * height, 
-    }; 
-  };
   displayFaceBox = (box) => { 
     this.setState({ box: box }); 
   };
