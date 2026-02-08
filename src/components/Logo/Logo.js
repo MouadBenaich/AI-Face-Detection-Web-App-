cfb20bd51fb45356fc08e17955fb3bfd996e3b1c
MouadBenaich <mobenchiso@gmail.com>
2026-02-08 15:27:39 +0100

    Logo maging bug fixed

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <div className='ma4 mt0'>
+        <div className='ma4-mt0'>
             <Tilt>
                 <div className="logo-container">
                     <img alt='logo' src={brain}/>
